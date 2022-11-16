f9f3c5d8a7d5ec2965afcc2d5ecaf34b28ea275d
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2022-11-16 00:02:31 +0000

    Combat system done

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject battlePanel;
     [SerializeField] private GameObject victoryPanel;
+    [SerializeField] private Text victoryText;
 
     [SerializeField] private Text playerPunkCount;
     [SerializeField] private Text playerMercCount;
@@ -49,6 +50,18 @@ public class GameManager : MonoBehaviour
         {
             battlePanel.SetActive(true);
 
+        }
+        else
+        {
+            battlePanel.SetActive(false);
+        }
+
+        if (victoryPanel.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                victoryPanel.SetActive(false);
+            }
         }
 
         ManagePerspective();
@@ -108,14 +121,113 @@ public class GameManager : MonoBehaviour
 
     public void CalcNextBattleStep()
     {
-        // Get player & enemy troop stats
+        Debug.Log("CalcNextBattleStep");
+
+        /* How should calculation work?
+         * 
+         * punks == 2 strength, must attack punks and mercs if present
+         * Mercs == 4 strength, must attack punks and mercs if present
+         * Hackers == 4 strength, if hacker is in party, can kill cyborgs and other hackers
+         * Cyborgs == 8 strength, can kill everything
+         * 
+         * greater totalStrength == advatange on rolls
+         * greater totalCount == advantage on rolls
+        */
+
         // Perform calc to determine losses
-        // Reflect losses in stats and UI
+        // calc player advantages
+        bool playerHasStrengthAdvantage = false; // determines whether player has strength advantage
+        bool playerHasNumbersAdvantage = false; // determines whether player has numbers advantage
+        int playerTotalStrength = playerArmy.punks * 2 + playerArmy.mercs * 4 + playerArmy.hackers * 4 + playerArmy.cyborgs * 8;
+        int enemyTotalStrength = enemyArmy.punks * 2 + enemyArmy.mercs * 4 + enemyArmy.hackers * 4 + enemyArmy.cyborgs * 8;
+        if (playerTotalStrength > enemyTotalStrength){ playerHasStrengthAdvantage = true; }
+        if (playerArmy.totalTroops > enemyArmy.totalTroops) { playerHasNumbersAdvantage = true; }
+
+        // Enemy losses
+        if (playerHasStrengthAdvantage && playerHasNumbersAdvantage)
+        {
+            enemyArmy.punks -= GenerateRandomTroopLoss(3); 
+            enemyArmy.mercs -= GenerateRandomTroopLoss(3);
+            enemyArmy.hackers -= GenerateRandomTroopLoss(3);
+            if (playerArmy.hackers > 0 || playerArmy.cyborgs > 0)
+            {
+                enemyArmy.cyborgs -= GenerateRandomTroopLoss(3);
+            }
+
+            playerArmy.punks -= GenerateRandomTroopLoss(1);
+            playerArmy.mercs -= GenerateRandomTroopLoss(1);
+            playerArmy.hackers -= GenerateRandomTroopLoss(1);
+            if (enemyArmy.hackers > 0 || enemyArmy.cyborgs > 0)
+            {
+                playerArmy.cyborgs -= GenerateRandomTroopLoss(1);
+            }
+                
+        }
+        else if (playerHasStrengthAdvantage || playerHasNumbersAdvantage)
+        {
+            enemyArmy.punks -= GenerateRandomTroopLoss(2);
+            enemyArmy.mercs -= GenerateRandomTroopLoss(2);
+            enemyArmy.hackers -= GenerateRandomTroopLoss(2);
+            if (playerArmy.hackers > 0 || playerArmy.cyborgs > 0)
+            {
+                enemyArmy.cyborgs -= GenerateRandomTroopLoss(2);
+            }
+
+            playerArmy.punks -= GenerateRandomTroopLoss(2);
+            playerArmy.mercs -= GenerateRandomTroopLoss(2);
+            playerArmy.hackers -= GenerateRandomTroopLoss(2);
+            if (enemyArmy.hackers > 0 || enemyArmy.cyborgs > 0)
+            {
+                playerArmy.cyborgs -= GenerateRandomTroopLoss(2);
+            }
+        }
+        else
+        {
+            enemyArmy.punks -= GenerateRandomTroopLoss(1);
+            enemyArmy.mercs -= GenerateRandomTroopLoss(1);
+            enemyArmy.hackers -= GenerateRandomTroopLoss(1);
+            if (playerArmy.hackers > 0 || playerArmy.cyborgs > 0)
+            {
+                enemyArmy.cyborgs -= GenerateRandomTroopLoss(1);
+            }
+
+            playerArmy.punks -= GenerateRandomTroopLoss(3);
+            playerArmy.mercs -= GenerateRandomTroopLoss(3);
+            playerArmy.hackers -= GenerateRandomTroopLoss(3);
+            if (enemyArmy.hackers > 0 || enemyArmy.cyborgs > 0)
+            {
+                playerArmy.cyborgs -= GenerateRandomTroopLoss(3);
+            }
+        }
+
+        // Reflect losses UI
+        playerArmy.EnsureCorrectTroopNumbers();
+        enemyArmy.EnsureCorrectTroopNumbers();
+        SetBattleUIStats();
+
         // Check if one party was victorious
+        if (playerArmy.totalTroops <= 0)
+        {
+            EndBattle();
+            victoryText.text = "A pathetic display!";
+            victoryPanel.SetActive(true);
+        }
+        else if (enemyArmy.totalTroops <= 0)
+        {
+            EndBattle();
+            victoryText.text = "You are Victorious!!";
+            victoryPanel.SetActive(true);
+        }
     }
 
     public void EndBattle()
     {
         Debug.Log("Battle over");
+        isBattling = false;
+    }
+
+    public int GenerateRandomTroopLoss(int max)
+    {
+        return Random.Range(0, max + 1);
     }
 }
